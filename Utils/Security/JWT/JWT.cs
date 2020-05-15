4cfcea9897791779d5a8e312f657f364fae12fd0
blasonrcelen <filipesantos__12@hotmail.com>
2020-05-15 20:25:44 +0100

    update

---
+++ Utils/Security/JWT/JWT.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Utils.Security.JWT
+{
+    public class JWT
+    {
+        public byte[] Key { get; set; }
+        public String Algorithm { get; set; }
+        public DateTime Expires { get; set; }
+
+        public JWT(byte[] key, String algorithm, DateTime expires)
+        {
+            Key = key;
+            Algorithm = algorithm;
+            Expires = expires;
+        }
+
+        public String GetToken(String identifier, String role)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, identifier),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = this.Expires,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Key), Algorithm)
+            };
+            return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+        }
+    }
+}
