23bc60b0b18ab722f97ba1f765bba1b2544fecce
blasonrcelen <filipesantos__12@hotmail.com>
2020-12-02 10:30:43 +0000

    commit

--- TestUtils/Program.cs
+++ TestUtils/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using Utils.Info.Log;
+using Utils.Info.App;
 
 namespace TestUtils
 {
     class Program
     {
-        private static readonly Log log = new Log("log4net.config");
-
         static void Main()
         {
-            // Log some things
-            log.Debug("Debug", new Exception("OLA MUNDo"));
-            log.Info("Hello logging world!");
-            log.Error("Error!");
-            log.Warn("Warn!");
         }
     }
 }
--- Utils/Info/Log/Log.cs
+++ Utils/Info/Log/Log.cs
@@ -7,59 +7,66 @@ using System.Runtime.CompilerServices;
 
 namespace Utils.Info.Log
 {
-    public class Log
+    public static class Log
     {
-        public Log(string fileName)
+        public const string DEFUALT_FILENAME = "log4net.config";
+
+        static Log()
         {
-            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()), new FileInfo(fileName));
+            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()), new FileInfo(DEFUALT_FILENAME));
         }
 
-        public void Debug(object message, [CallerMemberName] string fileName = "")
+        public static void SetLogConfigFile(string file)
+        {
+            XmlConfigurator.Configure(LogManager.GetRepository(Assembly.GetEntryAssembly()), new FileInfo(file));
+        }
+
+        public static void Debug(object message, [CallerMemberName] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Debug(message);
         }
 
-        public void Debug(object message, Exception exception, [CallerFilePath] string fileName = "")
+        public static void Debug(object message, Exception exception, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Debug(message, exception);
         }
 
-        public void Error(object message, [CallerFilePath] string fileName = "")
+        public static void Error(object message, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Error(message);
         }
 
-        public void Error(object message, Exception exception, [CallerFilePath] string fileName = "")
+        public static void Error(object message, Exception exception, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Error(message, exception);
         }
 
-        public void Fatal(object message, [CallerFilePath] string fileName = "")
+        public static void Fatal(object message, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Fatal(message);
         }
 
-        public void Fatal(object message, Exception exception, [CallerFilePath] string fileName = "")
+        public static void Fatal(object message, Exception exception, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Fatal(message, exception);
         }
 
-        public void Info(object message, [CallerFilePath] string fileName = "")
+        public static void Info(object message, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Info(message);
         }
 
-        public void Info(object message, Exception exception, [CallerFilePath] string fileName = "")
+        public static void Info(object message, Exception exception, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Info(message, exception);
         }
 
-        public void Warn(object message, [CallerFilePath] string fileName = "")
+        public static void Warn(object message, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Debug(message);
         }
 
-        public void Warn(object message, Exception exception, [CallerFilePath] string fileName = "")
+        public static void Warn(object message, Exception exception, [CallerFilePath] string fileName = "")
         {
             LogManager.GetLogger(Assembly.GetEntryAssembly(), fileName).Debug(message, exception);
         }
